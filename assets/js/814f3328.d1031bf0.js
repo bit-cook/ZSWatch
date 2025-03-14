f3f9e75d29c79070b29d8ba7247dab04efb26213
Jakob Krantz <mail@jakobkrantz.se>
2025-03-14 14:47:02 +0100

    Deploy website - based on 08ca178a83a39be7dc1e666f5bddd6fcfee95edb

--- assets/js/814f3328.d1031bf0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Funding","permalink":"/ZSWatch/blog/funding","unlisted":false,"date":"2025-03-06T00:00:00.000Z"}]}')}}]);
--- assets/js/91892b7b.b2a682a8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[823],{6315:t=>{t.exports=JSON.parse('{"tags":[{"label":"ZSWatch","permalink":"/ZSWatch/blog/tags/zswatch","description":"ZSWatch core","count":1},{"label":"NLnet Foundation","permalink":"/ZSWatch/blog/tags/nlnet","description":"Anything regarding NLnet Foundation","count":1}]}')}}]);
