6acd1ffb188b089568b11f1076de70d13a835ef3
Jakob Krantz <mail@jakobkrantz.se>
2025-03-14 17:17:21 +0100

    Deploy website - based on 5a14bce5ce2e3498d977a0de06a5ff5815c790b8

---
+++ assets/js/acecf23e.bae8d0ba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/ZSWatch/blog","blogTitle":"Blog","authorsListPath":"/ZSWatch/blog/authors"}')}}]);
